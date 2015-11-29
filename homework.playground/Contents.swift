fe0996031350aa0f2d0d8e39dbeb39204b8e56a2
Stefano Pezzino <spezzino.13@gmail.com>
2015-11-29 15:25:55 -0300

    homework

---
+++ homework.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum Velocidades: Int{
+    case Apagado = 0
+    case VelocidadBaja = 20
+    case VelocidadMedia = 50
+    case VelocidadAlta = 120
+    
+    init(velocidadInicial: Velocidades){
+        self = velocidadInicial
+    }
+    
+    var description : String {
+        switch self {
+        case .Apagado: return "Apagado";
+        case .VelocidadBaja: return "Velocidad Baja";
+        case .VelocidadMedia: return "Velocidad Media";
+        case .VelocidadAlta: return "Velocidad Alta";
+        }
+    }
+}
+
+class Auto{
+    var velocidad: Velocidades
+    
+    init(){
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad( ) -> ( actual : Int, velocidadEnCadena: String){
+        switch(self.velocidad){
+        case .Apagado:
+            self.velocidad = .VelocidadBaja
+        case .VelocidadBaja:
+            self.velocidad = .VelocidadMedia
+        case .VelocidadMedia:
+            self.velocidad = .VelocidadAlta
+        case .VelocidadAlta:
+            self.velocidad = .VelocidadMedia
+        }
+        
+        return (self.velocidad.rawValue, self.velocidad.description)
+    }
+}
+
+let auto: Auto = Auto()
+auto.velocidad
+for i in 1...20 {
+    let result = auto.cambioDeVelocidad()
+    print("Velocidad: ",result.actual, "\t", result.velocidadEnCadena)
+}
